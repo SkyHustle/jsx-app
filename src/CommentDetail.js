a628eb45dc948ef03a423e7de54ab0bad86fbdcd
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-03 15:32:07 -1000

    Create a comment detail component

---
+++ src/CommentDetail.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import faker from 'faker';
+
+const CommentDetail = ({ author, time, comment}) => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {author}
+          </a>
+          <div className="metadata">
+            <span className="date">{time}</span>
+          </div>
+          <div className="text">{comment}</div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Samski
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 8:00PM</span>
-          </div>
-          <div className="text">Cool ass blog post</div>
-        </div>
-      </div>
-    </div>
+    <CommentDetail
+      author={'Chuck Norris'}
+      time={'Today at 4:00PM'}
+      comment={'Cool ass comment'}
+    />
   )
 };
 
