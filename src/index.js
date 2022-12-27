0085381da966e0dda12364db7a2e6b95330357b4
Dmitry Sky <dmitry.sky@icloud.com>
2022-12-27 12:17:18 -0700

    Refector logic out of index and into separate App component

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
-import faker from 'faker';
-
-function App() {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Chuck Norris"
-          time="Today at 4:00PM"
-          content="Cool ass comment"
-          avatar={faker.image.avatar()} />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Michael Jackson"
-          time="Yesterday at 1:00PM"
-          content="bomb ass comment"
-          avatar={faker.image.avatar()} />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Yo Mama"
-          time="last year at 11:00AM"
-          content="yo mama comment"
-          avatar={faker.image.avatar()} />
-      </ApprovalCard>
-    </div>
-  );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'))
